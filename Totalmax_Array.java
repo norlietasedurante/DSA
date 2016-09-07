ea499d925300125e9a28f9cc1e6d1ab042892e39
norlietasedurante <norlietasedurante@gmail.com>
2016-09-07 23:14:49 +0800

    Add files via upload

---
+++ Totalmax_Array.java
@@ -0,0 +1,97 @@
+
+package totalmax_array;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Totalmax_Array {
+
+    
+
+    
+   int Array[];
+    int sum;
+    int max;
+    int capacity; 
+    int arraysize;
+   
+    
+
+   public void show (){
+       Scanner input = new Scanner (System.in);
+       Random  nory = new Random();
+       System.out.print("enter number:");
+       arraysize = input.nextInt();
+       Array = new int [arraysize];
+        
+       for (int i = 1; i < arraysize; i++) {
+         
+         if (i%10==0){
+             System.out.println("\n");
+               
+         } 
+         else{
+             Array[i] = nory.nextInt(1000);
+             System.out.print("\t" + Array[i]);
+             
+         }
+          
+    
+    }
+    System.out.println();
+  }
+   
+   public void getsum(){
+       for ( int i =0; i<arraysize; i++)
+       {
+           sum +=Array[i];
+       }
+       System.out.println("Sum " + sum);
+       
+   }
+   public void getAve (){
+        for (int  b=0; b < arraysize; b++){
+            double average = sum /Array.length;
+        }
+        System.out.println("Ave " + sum/Array.length);
+   }
+   
+   public void getmax(){
+       for (int i =0; i< arraysize; i++)
+       {
+           if (Array[i]> max ) 
+                max = Array[i];
+       }
+       System.out.println("Max " + max);
+   }
+   
+   
+    public static void main(String[] args) {
+  
+    
+      Totalmax_Array A= new Totalmax_Array();
+      
+      
+    
+    System.out.println();
+    
+    A.show();
+     System.out.println();
+     
+    A.getsum();
+    A.getmax();
+    A.getAve();
+   
+  
+    
+    }
+
+
+    
+}
+
+    public static void main(String[] args) {
+   
+    }
+    
+}
